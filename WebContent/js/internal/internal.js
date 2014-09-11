4ae442eaeefe870eec641e722f45d28b6be72e74
philm75 <phil.merrilees@gmail.com>
2014-09-11 23:34:52 +0100

    Change hyper links to use slide toggle, removed buttons.

--- WebContent/js/internal/internal.js
+++ WebContent/js/internal/internal.js
@@ -3,33 +3,28 @@ $(function() {
 	$("#sd-more-info").hide();
 			
 	$('#service-more-info-link').click(function(){
-		$("#service-more-info").toggle();
+		$("#service-more-info").slideToggle("fast", function() {
+			if ($("#service-more-info").is(":hidden")) {
+				$("#service-more-info-link").attr('value', 'Read more');
+				$("#service-more-info-link").text('Read more');
+			} else {
+				$("#service-more-info-link").attr('value', 'Read less');
+				$("#service-more-info-link").text('Read less');
+			}			
+		});
 	});
 
 	$('#sd-more-info-link').click(function(){
-		$("#sd-more-info").toggle();
+		$("#sd-more-info").slideToggle("fast", function() {
+			if ($("#sd-more-info").is(":hidden")) {
+				$("#sd-more-info-link").attr('value', 'Read more');
+				$("#sd-more-info-link").text('Read more');
+			} else {
+				$("#sd-more-info-link").attr('value', 'Read less');
+				$("#sd-more-info-link").text('Read less');
+			}					
+		});
 	});
-	
-	$("#service-more-info-link").on("click", function() {
-		if ($("#service-more-info").is(":hidden")) {
-			$("#service-more-info-link").attr('value', 'More Info');
-			$("#service-more-info-link").text('More Info');
-		} else {
-			$("#service-more-info-link").attr('value', 'Less Info');
-			$("#service-more-info-link").text('Less Info');
-		}
-	});
-	
-
-	$("#sd-more-info-link").on("click", function() {
-		if ($("#sd-more-info").is(":hidden")) {
-			$("#sd-more-info-link").attr('value', 'More Info');
-			$("#sd-more-info-link").text('More Info');
-		} else {
-			$("#sd-more-info-link").attr('value', 'Less Info');
-			$("#sd-more-info-link").text('Less Info');
-		}		
-	});	
 });
 
 
